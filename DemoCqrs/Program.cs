bc35ed5d77324833baef019f696b60ae350c6bfe
Paul Karam <paulkaram1989@hotmail.com>
2025-06-12 22:03:45 +0200

    Update Program.cs

--- DemoCqrs/Program.cs
+++ DemoCqrs/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddCQRSLibrary(Assembly.GetAssembly(typeof(GetUserQuery))!);
+builder.Services.AddCommandQueryLibrary(Assembly.GetAssembly(typeof(GetUserQuery))!);
 
 var app = builder.Build();
 
